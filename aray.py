e05c86494c415a280fc7f74297b5d8ed301f8685
Preethi <preethikondabathini@gmail.com>
2023-01-30 12:24:13 +0530

    initial commit

---
+++ aray.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.array([1,2,3,4],dtype=int)
+print(a)
+s=2**2**1%5
+print(s)
+b=3
+c=5
+d=print(b/c)
+type(d)
+x=
---
+++ stack.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0a13a3b1",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#stack\n",
+    "stack=[]\n",
+    "stack.append(1)\n",
+    "stack.append(2)\n",
+    "stack.append(3)\n",
+    "stack.append(4)\n",
+    "stack.append(5)\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
